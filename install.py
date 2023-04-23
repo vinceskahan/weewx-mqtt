7491b024d966f4f636e5c72e2fe17fba37697fab
vinceskahan <vince@skahan.net>
2023-04-23 12:09:28 -0700

    Update install.py
    tweak version string to make it clear this is a hacked fork

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ def loader():
 class MQTTInstaller(ExtensionInstaller):
     def __init__(self):
         super(MQTTInstaller, self).__init__(
-            version="0.24",
+            version="0.24vds",
             name='mqtt',
             description='Upload weather data to MQTT server.',
             author="Matthew Wall",
